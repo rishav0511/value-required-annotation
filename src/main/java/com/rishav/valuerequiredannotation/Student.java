ef30bdaceb1343e83aaf48f3cb04bfa653c913a7
rishav0511 <rishavraj0511@gmail.com>
2022-09-01 10:53:14 +0530

    @value

--- src/main/java/com/rishav/valuerequiredannotation/Student.java
+++ src/main/java/com/rishav/valuerequiredannotation/Student.java
@@ -1,33 +1,18 @@
 package com.rishav.valuerequiredannotation;
 
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.beans.factory.annotation.Value;
+
 public class Student {
+    @Value("${student.name}")
     private String name;
+
+    @Value("${student.course}")
     private String course;
+
+    @Value("${student.hobby}")
     private String hobby;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCourse() {
-        return course;
-    }
-
-    public void setCourse(String course) {
-        this.course = course;
-    }
-
-    public String getHobby() {
-        return hobby;
-    }
-
-    public void setHobby(String hobby) {
-        this.hobby = hobby;
-    }
 
     public void displayStudentInfo() {
         System.out.println("Name: "+name);
